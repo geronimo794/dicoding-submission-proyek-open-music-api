bc40976aeceeb4936387385734b3629c4846528c
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-08-20 22:38:07 +0700

    fix: Token manager

---
+++ src/tokenize/TokenManager.js
@@ -0,0 +1,22 @@
+import {Jwt} from '@hapi/jwt';
+import ResponseHelper from '../../utils/ResponseHelper.js';
+import InvariantError from './InvariantError.js';
+
+const TokenManager = {
+	generateAccessToken: (payload) =>
+		Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
+	generateRefreshToken: (payload) =>
+		Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+	verifyRefreshToken: (refreshToken) => {
+		try {
+			const artifacts = Jwt.token.decode(refreshToken);
+			Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+			const {payload} = artifacts.decoded;
+			return payload;
+		} catch (error) {
+			throw new InvariantError(ResponseHelper.RESPONSE_INVALID_INPUT);
+		}
+	},
+};
+
+export default TokenManager;
